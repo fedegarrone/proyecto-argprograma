bf3b517e6cf8d9f1f46129c956c4d2f55157a035
Federico Garrone <garronefederico97@gmail.com>
2023-01-31 21:37:20 -0300

    Se corrigieron problemas de diseño. Se puso imagen de fondo móvil. Se cambió foto de perfil y se quitó logo UNR

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ document.getElementById("slid-2").addEventListener("click", function () {
   document.getElementById("educ-formal").style.opacity = 0;
   document.getElementById("slid-1").style.backgroundColor = "var(--azul-fondo)";
   document.getElementById("educ-informal").style.opacity = 1;
-  document.getElementById("slid-1").style.backgroundColor = "var(--azul-fondo)";
   document.getElementById("slid-2").style.backgroundColor =
     "var(--azul-fuente)";
 });
